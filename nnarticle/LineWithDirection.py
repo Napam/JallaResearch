fix sign mistake hehe

nnarticle/LineWithDirection.py
@@ -3,23 +3,25 @@ import numpy as np
 from numpy.typing import ArrayLike
 
 
-def plotHyperplane(xspace: ArrayLike, intercept: float, xslope: float, yslope: float, n: int = 3, ax: plt.Axes = None, **kwargs):
+def plot_hyperplane(xspace: ArrayLike, intercept: float, xslope: float, yslope: float, n: int = 3, ax: plt.Axes = None, **kwargs):
     ax = ax or plt.gca()
     xspace = np.array(xspace)
-    a, b = - xslope / yslope, intercept / yslope
+    a, b = - xslope / yslope, - intercept / yslope
     f = lambda x: a * x + b
     plt.plot(xspace, f(xspace))
     xmin, xmax = xspace.min(), xspace.max()
     diff = xmax - xmin
     arrowxs = np.linspace(xmin + diff * 0.15, xmax - diff * 0.15, n)
-    plt.quiver(arrowxs, f(arrowxs), xslope, yslope, units='dots')
+    plt.quiver(arrowxs, f(arrowxs), xslope, yslope, **kwargs)
     return ax
 
 
 if __name__ == '__main__':
     xspace = np.linspace(-10, 10, 10)
-    ax = plotHyperplane(xspace, 0, 1, 1, width=0.1)
-    ax = plotHyperplane(xspace, -2, 1, -1, n=5, width=0.1)
+    ax = plot_hyperplane(xspace, 0, 1, 1, n=10, units='dots')
+    ax = plot_hyperplane(xspace, -2, 1, -1, n=10, units='dots')
+    ax = plot_hyperplane(xspace, -4, -2, -2, n=10, units='dots')
+    ax = plot_hyperplane(xspace, -6, -2, 2, n=10, units='dots')
     ax.set_aspect('equal')
     # ax.set_xlim((-10,10))
     # ax.set_ylim((-10,10))

nnarticle/OnePlaneModel.py
@@ -6,7 +6,7 @@ from torch import optim
 from matplotlib import pyplot as plt
 
 from utils import get_lims
-from LineWithDirection import plotHyperplane
+from LineWithDirection import plot_hyperplane
 
 def mse(y_: torch.Tensor, y: torch.Tensor):
     assert y_.shape == y.shape
@@ -61,15 +61,16 @@ class OnePlaneModel(nn.Module):
         print(a, b)
         plt.scatter(*X.T, c=y)
 
-        intercept_ = - intercept + (m_1 * xslope) / s_1 + (m_2 * yslope) / s_2
+        intercept_ = intercept - (m_1 * xslope) / s_1 - (m_2 * yslope) / s_2
+        # intercept_ = - intercept + (m_1 * xslope) / s_1 + (m_2 * yslope) / s_2
         xslope_ = xslope / s_1
         yslope_ = yslope / s_2
-        plotHyperplane(xspace, intercept_, xslope_, yslope_, 16)
-        # plt.plot(xspace, a * xspace + b)
+        plot_hyperplane(xspace, intercept_, xslope_, yslope_, 16, units='dots')
+        plt.plot(xspace, a * xspace + b)
 
         xlim, ylim = get_lims(X, padding = 0.5)
-        plt.xlim(*xlim)
-        plt.ylim(*ylim)
+        # plt.xlim(*xlim)
+        # plt.ylim(*ylim)
         plt.gca().set_aspect('equal')
         plt.show()
 

nnarticle/apples_and_oranges_viz.py
@@ -2,12 +2,12 @@ import torch
 import pandas as pd
 from matplotlib import pyplot as plt
 
-from utils import get_lims, plotHyperplane
+from utils import get_lims, plot_hyperplane
 plt.rcParams.update({'font.family': 'serif', 'mathtext.fontset':'dejavuserif'})
 
 df = pd.read_csv("datasets/apples_and_oranges.csv")
 X, y = df[["weight", "height"]].values, df["class"].map({"apple": 0, "orange": 1}).values
-x_lim, y_lim = get_lims(X)
+x_lim, y_lim = get_lims(X, padding = 2.0)
 
 def visualize_data_set():
     plt.xlabel("Weight (g)")
@@ -44,13 +44,21 @@ def visualize_data_set_with_unknown_point_and_line():
     intercept = -0.009632732719182968
     xslope = -0.93068683
     yslope = -1.2795106
-    a, b = - xslope / yslope, intercept / yslope
-    xrange = torch.linspace(x_lim[0] * 0.5, x_lim[1] * 1.5, 4) 
-    plt.plot(xrange, a * xrange + b, c='k', label='Decision boundary')
+    s_1, s_2 = 7.5490, 1.5144
+    m_1, m_2 = 135.9769, 6.8887
+
+
+
+    xspace = torch.linspace(x_lim[0] * 0.5, x_lim[1] * 1.5, 4) 
+    intercept_ = - intercept + (m_1 * xslope) / s_1 + (m_2 * yslope) / s_2
+    xslope_ = xslope / s_1
+    yslope_ = yslope / s_2
+    plot_hyperplane(xspace, intercept_, xslope_, yslope_, 10)
     plt.legend(loc="upper right")
     plt.xlim(*x_lim)
     plt.ylim(*y_lim)
     # plt.savefig("figures/applesnoranges_unknown_point_with_line.pdf")
+    plt.gca().set_aspect('equal')
     plt.show()
     plt.clf()
 

nnarticle/generate_apples_and_oranges.py
@@ -29,7 +29,7 @@ X, y = make_blobs([30, 30], centers, [[6,1], [6,1]])
 df = pd.DataFrame({
     "weight": X[:,0],
     "height": X[:,1],
-    "class": np.vectorize({0: "apple", 1: "orange"}.get)(y)
+    "class": np.vectorize({0: "orange", 1: "apple"}.get)(y)
 })
 print(df)
 df.to_csv("datasets/apples_and_oranges.csv", index=False)

nnarticle/utils.py
@@ -1,11 +1,12 @@
 import torch
 from matplotlib import pyplot as plt
+from numbers import Number
 from numpy.typing import ArrayLike
 import numpy as np
 
 def get_lims(X: torch.Tensor, padding: float | ArrayLike = 0.25):
-    if isinstance(padding, float):
-        padding = np.array([padding] * 2)
+    if isinstance(padding, Number):
+        padding = np.array([padding, padding])
 
     x_min, x_max = X[:,0].min(), X[:,0].max()
     y_min, y_max = X[:,1].min(), X[:,1].max()
@@ -13,7 +14,7 @@ def get_lims(X: torch.Tensor, padding: float | ArrayLike = 0.25):
     return (x_min - x_std * padding[0], x_max + x_std * padding[0]), (y_min - y_std * padding[1], y_max + y_std * padding[1])
 
 
-def plotHyperplane(xspace: ArrayLike, intercept: float, xslope: float, yslope: float, n: int = 3, ax: plt.Axes = None, **kwargs):
+def plot_hyperplane(xspace: ArrayLike, intercept: float, xslope: float, yslope: float, n: int = 3, ax: plt.Axes = None, **kwargs):
     ax = ax or plt.gca()
     xspace = np.array(xspace)
     a, b = - xslope / yslope, intercept / yslope