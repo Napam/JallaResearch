7a8e2587255cc57ffc4cbfe8d870d1723022fa9b
Naphat Amundsen <naphat@live.no>
2023-03-04 22:26:17 +0100

    dictdiff tests

--- dictdiff/test.py
+++ dictdiff/test.py
@@ -1,85 +1,241 @@
 from typing import Dict, Hashable, Union, Tuple, Any
 from pprint import pprint
+from copy import deepcopy
+import unittest
 
 
-def items(d: Dict):
+class DictTuple(tuple):
+    """Used for separating from regular tuples"""
+
+    def __repr__(self):
+        return f"#{super().__repr__()[1:-1]}#"
+
+
+class _Undefined:
+    """Flag for non-existing value"""
+
+    def __repr__(self):
+        return "undefined"
+
+
+undefined = _Undefined()
+
+
+def recursiveitems(d: Dict):
     for key, val in d.items():
-        if isinstance(val, dict):
-            yield (key, tuple(items(val)))
+        if type(val) == dict:
+            yield (key, DictTuple(recursiveitems(val)))
+        elif type(val) == list:
+            yield (key, tuple(val))
         else:
             yield (key, val)
 
 
-def diff(a: Dict, b: Dict):
-    a_set = set(items(a))
-    b_set = set(items(b))
+def difference(a: Dict, b: Dict):
+    a_set = set(recursiveitems(a))
+    b_set = set(recursiveitems(b))
     diffs = a_set.symmetric_difference(b_set)
-    # return {key: (a.get(key, None), b.get(key, None)) for key, val in diff}
     changes = {}
     for key, val in diffs:
-        if isinstance(val, tuple):
-            changes[key] = diff(a.get(key, None), b.get(key, None))
+        if type(val) == DictTuple:
+            changes[key] = difference(a.get(key, undefined), b.get(key, undefined))
         else:
-            changes[key] = (a.get(key, None), b.get(key, None))
+            changes[key] = (a.get(key, undefined), b.get(key, undefined))
 
     return changes
 
 
-def diffupdate(
+def _apply(
     d: Dict,
     changes: Dict[Hashable, Union[Tuple[Any, Any], Dict]],
+    delete: bool,
+    add: bool,
+    update: bool,
+):
+    for key, val in changes.items():
+        if type(val) == dict:
+            _apply(d.get(key), val, delete, add, update)
+            continue
+
+        before, after = val
+        if delete and (after is undefined):
+            del d[key]
+            continue
+
+        if (add and (before is undefined)) or update:
+            d[key] = after
+            continue
+
+    return d
+
+
+def apply(
+    d: Dict,
+    diff: Dict[Hashable, Union[Tuple[Any, Any], Dict]],
+    *,
     delete: bool = True,
     add: bool = True,
     update: bool = True,
-) -> None:
-    for key, val in changes.items():
-        if isinstance(val, dict):
-            diffupdate(d.get(key), val, delete, add, update)
-        else:
-            before, after = val
-            del_ = after is None
-            add_ = before is None
-
-            if delete and del_:
-                del d[key]
-                continue
-
-            if add and add_:
-                d[key] = after
-                continue
-
-            if update and (not del_ and not add_):
-                d[key] = after
-                continue
+    mutate: bool = False,
+):
+    target = d if mutate else deepcopy(d)
+    result = _apply(target, diff, delete, add, update)
+    return None if mutate else result
 
 
-if __name__ == "__main__":
+class Test(unittest.TestCase):
+    def test_recursiveitems(self):
+        d = {
+            "a": 1,
+            "b": "A string",
+            "c": (1, 2, 3),
+            "d": [4, 5, 6],
+            "e": {
+                "f": None,
+            },
+        }
+
+        items = list(recursiveitems(d))
+        expected = [
+            ("a", 1),
+            ("b", "A string"),
+            ("c", (1, 2, 3)),
+            ("d", (4, 5, 6)),
+            (
+                "e",
+                DictTuple(
+                    [
+                        ("f", None),
+                    ]
+                ),
+            ),
+        ]
+
+        self.assertListEqual(items, expected)
+
     a = {
         "a": 1,
-        "b": 2,
-        "c": 3,
-        "e": {
-            "f": 1,
-            "g": {
-                "lol": "bananaa",
-            },
-        },
+        "b": "A string",
+        "c": (1, 2, 3),
+        "d": [1, 2, 3],
+        "e": {"f": 1, "g": {}},
+        "i": {"j": 1, "k": {"l": 0}},
+        "m": 123,
+        "n": frozenset((7, 6, 8)),
     }
 
     b = {
-        "a": 60,
-        "b": 3,
-        "d": 4,
-        "e": {
-            "f": 1,
-            "g": {
-                "lol": "banana",
-            },
-        },
+        "a": 1,
+        "b": "A string!!!",
+        "c": (1, 2, 3),
+        "d": [2, 4],
+        "e": {"f": 1, "g": {"h": "help"}},
+        "i": {"j": 43, "k": {"l": 0}},
+        "n": frozenset((9, 8)),
     }
 
-    res = diff(a, b)
+    diff = {
+        "b": ("A string", "A string!!!"),
+        "d": ([1, 2, 3], [2, 4]),
+        "e": {"g": {"h": (undefined, "help")}},
+        "i": {"j": (1, 43)},
+        "m": (123, undefined),
+        "n": (frozenset((7, 6, 8)), frozenset((9, 8))),
+    }
 
-    pprint(a, width=1)
-    diffupdate(a, res, delete=True, add=True, update=False)
-    pprint(a, width=1)
+    def test_diff(self):
+        diff = difference(self.a, self.b)
+        self.assertDictEqual(diff, self.diff)
+
+    def test_apply(self):
+        b = apply(self.a, self.diff)
+        self.assertDictEqual(b, self.b)
+
+    def test_apply_mutate(self):
+        a = deepcopy(self.a)
+        apply(a, self.diff, mutate=True)
+        self.assertDictEqual(a, self.b)
+
+    def test_apply_update_only(self):
+        a = {
+            "a": {
+                "delete": 1,
+                "update": 3,
+            },
+        }
+
+        diff = {
+            "a": {
+                "delete": (1, undefined),
+                "update": (3, 2),
+                "add": (undefined, 123),
+            }
+        }
+
+        b = apply(a, diff)
+        expected = {
+            "a": {"update": 2, "add": 123},
+        }
+
+        self.assertDictEqual(b, expected)
+
+    def test_apply_delete_only(self):
+        a = {
+            "a": {
+                "delete": 1,
+                "update": 3,
+            },
+        }
+
+        diff = {
+            "a": {
+                "delete": (1, undefined),
+                "update": (3, 2),
+                "add": (undefined, 123),
+            }
+        }
+
+        b = apply(a, diff, update=False, add=False)
+        expected = {
+            "a": {
+                "update": 3,
+            },
+        }
+
+        self.assertDictEqual(b, expected)
+
+    def test_apply_add_only(self):
+        a = {
+            "a": {
+                "delete": 1,
+                "update": 3,
+            },
+        }
+
+        diff = {
+            "a": {
+                "delete": (1, undefined),
+                "update": (3, 2),
+                "add": (undefined, 123),
+            }
+        }
+
+        b = apply(a, diff, update=False, delete=False)
+        expected = {
+            "a": {
+                "delete": 1,
+                "update": 3,
+                "add": 123,
+            },
+        }
+
+        self.assertDictEqual(b, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+    # res = diff(a, b)
+    # pprint(a, width=1)
+    # apply(a, res, delete=True, add=True, update=True)
+    # pprint(a, width=1)
