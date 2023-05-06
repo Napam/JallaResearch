126d1f98301faa41680dfc4df11bbe6410f427da
Naphat Amundsen <naphat@live.no>
2023-05-06 20:15:26 +0200

    refactor radix rust according to clippy

--- radix-rust/src/main.rs
+++ radix-rust/src/main.rs
@@ -22,11 +22,11 @@ impl Node {
         Node { children: None }
     }
 
-    fn update(&mut self, tokens: &Vec<&str>) {
+    fn update(&mut self, tokens: &[&str]) {
         let Some(&first) = tokens.first() else { return };
-        let mut map = self.children.get_or_insert_with(|| HashMap::new());
-        let child = map.entry(first.to_string()).or_insert(Node::new());
-        child.update(&tokens[1..].to_vec());
+        let mut map = self.children.get_or_insert_with(HashMap::new);
+        let child = map.entry(first.to_string()).or_insert_with(Node::new);
+        child.update(&tokens[1..]);
     }
 }
 
@@ -40,8 +40,18 @@ impl fmt::Debug for Index {
     }
 }
 
+fn _find_subpath_of(current: &Node, tokens: &[&str]) -> Option<String> {
+    let [token_of_child, rest @ ..] = tokens else { return None };
+
+    current.children.as_ref().map(|map| {
+        map.get(&token_of_child.to_string())
+            .and_then(|node| _find_subpath_of(node, rest))
+            .map_or(token_of_child.to_string(), |_| "aaa".to_string())
+    })
+}
+
 impl Index {
-    fn from(paths: &Vec<String>) -> Self {
+    fn from(paths: &[&str]) -> Self {
         let mut map: HashMap<String, Node> = HashMap::new();
 
         for path in paths.iter() {
@@ -52,49 +62,31 @@ impl Index {
                 continue;
             }
 
-            let mut node = map.entry(first.to_string()).or_insert_with(|| Node::new());
-            node.update(&rest.to_vec());
+            let mut node = map.entry(first.to_string()).or_insert_with(Node::new);
+            node.update(rest);
         }
 
         Index { map }
     }
 
-    fn _find_subpath_of(&self, current: &Node, tokens: &[&str]) -> Option<String> {
-        let [first, rest @ ..] = tokens else { return None };
-        match &current.children {
-            None => None,
-            Some(map) => map
-                .get(&first.to_string())
-                .and_then(|node| self._find_subpath_of(node, rest))
-                .map(|_| "aaa".to_string()),
-        }
-    }
-
-    fn find_subpath_of(&self, path: &String) -> Option<String> {
-        let tokens: Vec<&str> = path.split("/").collect();
+    fn find_subpath_of(&self, path: &str) -> Option<String> {
+        let tokens: Vec<&str> = path.split('/').collect();
         let [first, rest @ ..] = tokens.as_slice() else { return None };
 
         match self.map.get(&first.to_string()) {
             None => None,
-            Some(node) => self._find_subpath_of(node, rest)
+            Some(node) => _find_subpath_of(node, rest),
         }
     }
 }
 
 fn main() {
-    let paths = vec![
-        "a/b/c".to_string(),
-        "a/r/f".to_string(),
-        "a/r/f/d".to_string(),
-        "b/c".to_string(),
-        "c".to_string(),
-        "".to_string(),
-    ];
+    let paths = vec!["a/b/c", "a/r/f", "a/r/f/d", "b/c", "c", ""];
 
-    let index = Index::from(&paths);
-    println!("{:#?}", index);
+    let index = Index::from(paths.as_slice());
+    println!("LOG:\x1b[33mDEBUG\x1b[0m: index: {:#?}", index);
 
-    let sub = index.find_subpath_of(&"b/c".to_string());
+    let sub = index.find_subpath_of("a/r/f/d/asdf");
     // index.find_subpath_of(&"x/b/c/d/e".to_string());
-    println!("{:?}", sub);
+    println!("LOG:\x1b[33mDEBUG\x1b[0m: sub: {:?}", sub);
 }
